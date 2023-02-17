4533791fdb1555ca9095edc0777e37fef0182705
Rajit <rajitgola@hotmail.com>
2023-02-17 12:59:21 +0530

    Environment-ising the application, added favicon & created basic helper services along with constant files

---
+++ src/services/api.service.ts
@@ -0,0 +1,7 @@
+import { IAPIResponse } from "../shared/models";
+
+export function getCompanyInfo() : Promise<IAPIResponse<any>> {
+    return new Promise<IAPIResponse<any>>((resolve, reject) => {
+        
+    })
+}
