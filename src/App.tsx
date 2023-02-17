4e0a8b55231b5ca42e6fcc5f9ed54f05ebe9c7b2
Rajit <rajitgola@hotmail.com>
2023-02-17 12:47:12 +0530

    Created shared model interfaces & components for different pages & routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import About from './pages/About/About';
+import Login from './pages/Auth/Login';
+import Home from './pages/Home/Home';
+import Profile from './pages/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home"/>} />
+          <Route path="/home" element={ <Home/>} />
+          <Route path="/about" element={ <About/>} />
+          <Route path="/sign-in" element={ <Login/>} />
+          <Route path="/profile" element={ <Profile/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
